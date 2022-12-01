08804b04a518aa964f579c8f122528fce36393d0
Abraham Samuel Adekunle <sammymore50@gmail.com>
2022-12-01 15:10:05 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-	argc = len(sys.argv)
-	total = 0
-	for i in range(1, argc):
-		total += int(sys.argv[i])
-	print(total)
+    argc = len(sys.argv)
+    total = 0
+    for i in range(1, argc):
+        total += int(sys.argv[i])
+    print(total)
