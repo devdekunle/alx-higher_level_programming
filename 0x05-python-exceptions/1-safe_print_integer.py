967a25237f77e3d761c72cb731bf1fae30db9cd3
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-19 15:33:38 -0800

    safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except Exception as err
+    except Exception as error
         return False
     else:
         return True
