b0f5f8cc9b2686af00ec43cd6ef5fbdb486f8e4e
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-19 13:04:08 -0800

    correct pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	i = 0
-	for elem in my_list:
-		if (i < x):
-			try:
-				print("{}".format(my_list[i]), end="")
-				i += 1
-			except IndexError:
-				print("\n")
-	print("")
-	return i
+    i = 0
+    for elem in my_list:
+        if (i < x):
+            try:
+                print("{}".format(my_list[i]), end="")
+                i += 1
+            except IndexError:
+                print("\n")
+    print("")
+    return i
