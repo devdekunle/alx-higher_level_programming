db277341d62b73782c7840766d5bf631bb238495
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-19 13:31:29 -0800

    safe print integer

---
+++ 0x05-python-exceptions/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+
+value = 78
+safe_print_integer(value)
+value2 = "School"
+safe_print_integer(value2)
