f1074812282bb38f3c5e545cfbd47fda6b797285
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-25 15:19:04 -0800

    bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
 
 from math import pi
-	"""
-	This module produces the disassemly of a Magicclass
+"""
+This module produces the disassemly of a Magicclass
 
-	"""
+"""
 
 class MagicClass:
-	"""
-		definition of a Magic class
-	"""
-        def __init__(self, radius=0):
-			"""
-			defines an instance of a class
-			Arg:
-				radius(int): radius of a circle
-				Return: None
-			"""
-                if type(radius) is not int or type(radius) is not float:
-                        raise TypeError("radius must be a number")
-                self.__radius = radius
-                return None
-        def area(self, radius):
-			"""
-			defines the area 
-			Arg:
-				radius(int): radius of a circle
-				Return: area
-			"""
-                return (self.__radius ** 2) * pi
+    """
+    definition of a Magic class
+    """
+    def __init__(self, radius=0):
+        """
+        defines an instance of a class
+        Arg:
+           radius(int): radius of a circle
+            Return: None
+         """
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+            self.__radius = radius
+            return None
+    def area(self, radius):
+        """
+        defines the area 
+        Arg:
+            radius(int): radius of a circle
+            Return: area
+        """
+        return (self.__radius ** 2) * pi
 
-        def circumference(self, radius):	
-			"""
-			defines the circumference
-			Arg:
-				radius(int): radius of a circle
-				Return: circumference
-			"""
-                return (2 * pi * self.__radius)
+    def circumference(self, radius):	
+        """
+        defines the circumference
+        Arg:
+            radius(int): radius of a circle
+            Return: circumference
+        """
+
+        return (2 * pi * self.__radius)
 
---
+++ 0x06-python-classes/103-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import dis
+MagicClass = __import__('103-magic_class').MagicClass
+dis.dis(MagicClass)
