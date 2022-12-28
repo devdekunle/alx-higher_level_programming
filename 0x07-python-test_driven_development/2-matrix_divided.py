4469f2284952d9b645f6dec09f556b0fb1ddadd7
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-28 15:32:52 -0800

    matrix divide testing

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains a function that divides each element of a matrix by a number
+and returns a new matrix with the values of the divided elements.
+The function also considers different cases and also edge cases which could
+limit the effectiveness of the program.
+"""
+def matrix_divided(matrix, div):
+    """ This function divides each element of the matix by div
+    Arg: 
+    matrix: a list of lists
+    div: number to divide each element of the matrix
+    Return: new_matrix"""
+
+    if type(matrix) is not list[list[]]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    elif type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    else:
+        new_matrix = []
+
+        for row in matrix:
+            row_length = len(row)
+            new_row = []
+            for elem in row:
+                if elem not in [float, int]
+                    raise TypeError("element must be a float or an integer")
+                elif elem is None:
+                    raise TypeError("")
+                else:
+                    elem = elem / div
+                    new_row.append(elem)
+            new_matrix.append(new_row)
+        return new_matrix
