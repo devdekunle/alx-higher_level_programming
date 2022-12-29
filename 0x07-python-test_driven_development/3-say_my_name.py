def655589bea4323fb8de919ba1bef9635f6e7a1
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-29 13:03:34 -0800

    corrected say_my_name tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -18,4 +18,4 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise  TypeError("last_name must be a string")
     else:
-       print("{} {}".format(first_name, last_name))
+       print("My name is {} {}".format(first_name, last_name))
