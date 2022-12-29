216942f0e234e17a6f6bbfd58185894b04aaee25
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-29 12:24:58 -0800

    print square tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module cotains the function which prints a square based 
+    on the size that has been passed to the function.
+    It also checks all possible egde cases to make sure
+    that the function works effectively.
+"""
+
+def print_square(size):
+    """method which creates a square based on the size
+    Args:
+    size: the size of the square to print
+    Return: Nothing
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    elif size is None:
+        raise TypeError("print_square() missing 1 required positional arguement: 'size'")
+    else:
+        print(f"{size * '#'}")
