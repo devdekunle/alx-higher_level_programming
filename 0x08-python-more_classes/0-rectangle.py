475b166fd26fd196618d3d70e1bd04e5ac96d985
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-02 08:32:21 -0800

    intialising an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" This module contains the definition of an empty class Rectangle which 
+""" This module contains the definition of an empty class Rectangle which
 defines a rectangle
 """
 
+
 class Rectangle:
+
     """This class defines a rectangle
     """
     pass
