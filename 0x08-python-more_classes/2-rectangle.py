2e8a03f3cfed3a5407b3e5e0bd2a6ab7958d1688
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-02 10:26:00 -0800

    set perimeter and area of a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+""" This module contains the definition of an empty class Rectangle which
+defines a rectangle
+"""
+
+
+class Rectangle:
+
+    """This class defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        method to iniialize an instance of a class
+        Args:
+        width:set the width
+        height: set the height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ return the value of the width
+        Return: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width to a given value
+        Args:
+        value: value to set
+        Return: Nothing
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ return the value of the width
+        Return: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the height to a given value
+        Args:
+        value: value to set
+        Return: Nothing
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """This method returns the area of a rectangle
+        Args:
+        Return: Area of a rectangle
+        """
+        return (self.height * self.width)
+
+    def perimeter(self):
+        """This method gets the perimeter of arectangle and returns it
+        Args:
+        Return: perimeter
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.height + self.width)
