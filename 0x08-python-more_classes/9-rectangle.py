ef9f37e8b0ccf562fc4b6c2beedf088f06f59b15
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-02 16:33:38 -0800

    class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -125,7 +125,7 @@ class Rectangle:
                 return rect_1
             else:
                 return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         """This method returns rectangle with height equal width
