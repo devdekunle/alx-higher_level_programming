d92377f8fdbec1f86ee2d2ed233b072539d39617
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-04 02:35:18 -0800

    fixed pycode for matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-This module contains a function that divides each element of a matrix by a number
+This module contains a function that divides each element of a
+matrix by a number
 and returns a new matrix with the values of the divided elements.
 The function also considers different cases and also edge cases which could
 limit the effectiveness of the program.
 """
+
+
 def matrix_divided(matrix=None, div=None):
     """ This function divides each element of the matix by div
-    Arg: 
+    Arg:
     matrix: a list of lists
     div: number to divide each element of the matrix
     Return: new_matrix"""
 
     if type(matrix) is not list or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+        (list of lists) of integers/floats")
     elif type(div) not in [int, float]:
         raise TypeError("div must be a number")
     elif div == 0:
@@ -23,10 +27,11 @@ def matrix_divided(matrix=None, div=None):
         row_length = len(matrix[0])
         for row in matrix:
             if len(row) != row_length:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix \
+                must have the same size")
             new_row = []
             for elem in row:
-                
+
                 if type(elem) not in [float, int]:
                     raise TypeError("element must be a float or an integer")
                 else:
