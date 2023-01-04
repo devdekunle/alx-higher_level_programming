780f7e3a9dfa436f7ccaf29a1e8cde5e2bbdf2f9
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-04 03:49:22 -0800

    fixed say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """ This module print the name of the arguements of the function.
-It also checks all edge cases to 
+It also checks all edge cases to
 make sure that the function works
 efficiently.
 """
+
+
 def say_my_name(first_name, last_name=""):
-    """ the function prints thr first and last name while also checking all edge cases
+    """ the function prints thr first and last
+    name while also checking all edge cases
     Args:
     first_name: first name
     last_name: last name
     Return: Nothing
     """
     if first_name is None:
-        raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
+        raise TypeError(r"say_my_name() missing 1 required \
+        positional argument: 'first_name'")
     elif type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
-        raise  TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string")
     else:
-       print("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
