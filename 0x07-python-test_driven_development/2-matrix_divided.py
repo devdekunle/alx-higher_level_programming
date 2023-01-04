d3b3adf102272a22765b9dc2f9a0827b23306c53
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-04 03:02:29 -0800

    fixed matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,7 +16,8 @@ def matrix_divided(matrix=None, div=None):
     Return: new_matrix"""
 
     if type(matrix) is not list or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of \
+        integers/floats")
     elif type(div) not in [int, float]:
         raise TypeError("div must be a number")
     elif div == 0:
@@ -26,11 +27,13 @@ def matrix_divided(matrix=None, div=None):
         row_length = len(matrix[0])
         for row in matrix:
             if len(row) != row_length:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix must have the same \
+                size")
             new_row = []
             for elem in row:
                 if type(elem) not in [float, int]:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError("matrix must be a matrix (list of lists) of \
+                    integers/floats")
                 else:
                     elem = round((elem / div), 2)
                     new_row.append(elem)
