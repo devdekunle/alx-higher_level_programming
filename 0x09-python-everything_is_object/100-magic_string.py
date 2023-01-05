f2f0e842e091346062c82775865938913c9ec456
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-04 16:48:17 -0800

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(var={"num": 0}):
-   var["num"] += 1
-   return str("BestSchool, " * var["num"])[:-2]
+    var["num"] += 1
+    return str("BestSchool, " * var["num"])[:-2]
