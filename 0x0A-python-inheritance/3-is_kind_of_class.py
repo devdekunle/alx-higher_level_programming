62f97a17a438b14ee6f1c4a80f85775bbd02f288
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-10 05:43:32 -0800

    check instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module checks if an object is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns true or false"""
+    return (isinstance(obj, a_class))
