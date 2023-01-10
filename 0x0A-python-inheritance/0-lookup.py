a569ebc89b6ffc97afc90445f2cb01bf5159ee4c
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-09 22:19:23 -0800

    fixed pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,6 @@
 
 
 def lookup(obj):
-	""" This method returns all the attributest and methods present 
-		in obj"""
-        return dir(obj)
+    """ This method returns all the attributest and methods present
+        in obj"""
+    return dir(obj)
