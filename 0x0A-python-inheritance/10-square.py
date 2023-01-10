af6bc0ca186b201a71a6969aa47dbcdac9751426
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-10 12:08:37 -0800

    write a square class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Inheritance from class Ractangle"""
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """ a square class that inherits from Rectangle class"""
@@ -8,7 +8,8 @@ class Square(Rectangle):
         """instantiation of the square class"""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
     def area(self):
         """return the area of a square"""
         return (self.__size ** 2)
-        
+
