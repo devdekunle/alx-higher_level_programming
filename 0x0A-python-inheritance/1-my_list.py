45954895f5a899c29def1441b27fe4ad687db558
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-10 00:09:53 -0800

    sorted tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module is has a subclass that inherita a super class
+    """
+
+
+class MyList(list):
+    """This class deffines a method to print a sorted list
+    """
+    def __init__(self):
+        """initialize the object using the preferred class"""
+        super().__init__()
+
+
+    def print_sorted(self):
+        """This method returns a sorted list"""
+        print(sorted(self))
+        
+
