9aae5473f544a629e0f9edde76d3e009dafc5539
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-11 15:04:38 -0800

    load add and save using json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Module to load, add and save objects using json"""
-
-from sys import argv
+import sys
 import json
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -13,5 +12,5 @@ if os.path.isfile(filename):
     my_obj = load_from_json_file(filename)
 else:
     my_obj = []
-my_obj.extend(argv[1:])
+my_obj.extend(sys.argv[1:])
 save_to_json_file(my_obj, filename)
