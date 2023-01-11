03ff819acd162d51dc172be89384c0124b4b28c2
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-11 08:15:39 -0800

    write toa file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+""" Module to write to a file"""
+
+
+def write_file(filename="", text=""):
+    """ function to write toa file"""
+    with open(filename, mode="w", encoding="utf-8") as my_file:
+        my_file.write(text)
+
+    with open(filename, encoding="utf-8") as my_file:
+        line = my_file.read()
+    return len(line)
