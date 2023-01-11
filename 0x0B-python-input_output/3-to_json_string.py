35ec74f683f2768ed906255fb9e1df1b209215d8
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-11 09:02:46 -0800

    object serializing with json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """ Module to convert object into string representation using json"""
 import json
 
+
 def to_json_string(my_obj):
     """ returns the serialized object"""
     return json.dumps(my_obj)
