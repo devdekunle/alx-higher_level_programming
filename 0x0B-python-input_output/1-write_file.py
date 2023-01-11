c8eade2d13941e69187de1abc2c1cd44a8a9e56b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-11 08:27:50 -0800

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,4 @@
 def write_file(filename="", text=""):
     """ function to write toa file"""
     with open(filename, mode="w", encoding="utf-8") as my_file:
-        my_file.write(text)
-
-    with open(filename, encoding="utf-8") as my_file:
-        line = my_file.read()
-    return len(line)
+        return my_file.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to append to a file"""
+
+
+def append_write(filename="", text=""):
+    """return numbr of characters appended"""
+    with open(filename, mode="a", encoding="utf-8") as my_file:
+       len_append = my_file.append(text) 
+       return len_append
