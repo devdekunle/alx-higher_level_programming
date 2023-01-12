63bef01fffb661eb2cecc69ff4e4de6b2a611a58
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-11 20:40:46 -0800

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Module to serialize a class instance """
-import json
 
 
 def class_to_json(obj):
     """method to return the dictionary description with
     simple data structure"""
-    obj_str = json.dumps(obj.__dict__)
-    return obj_str
+    obj_dict = {}
+    if hasattr(obj, "__dict__"):
+        obj_dict = obj.__dict__.copy()
+    return obj_dict
