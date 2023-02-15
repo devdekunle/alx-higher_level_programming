268092989e1cac65e8c30d75332db611451887b3
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-02-15 17:48:46 +0100

    print the full description of the table first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- prints the full description of the table first_table
 -- in the database hbtn_0c_0
 -- the database name will passed as argument
-SHOW CREATE TABLE first_table
+SHOW CREATE TABLE first_table;
