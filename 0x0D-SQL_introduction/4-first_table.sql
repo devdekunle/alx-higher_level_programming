c5feda8e421a2148446291e0feebaca4d4fdb061
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-02-15 17:21:59 +0100

    create first table in a database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS
+    `first_table` (
+    `id` int(10) NOT NULL,
+    `name` VARCHAR(256)
+    ) ENGINE=InnoDB AUTO_INCREMENT=5;
