89ffdd64bb7f267092d2e9c5e2fc8bf554c6c916
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-14 00:49:07 +0100

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,12 @@ let args = parseInt(process.argv[2]);
 if (args.length < 3 || isNaN(args)) {
   console.log('Missing size');
 } else {
-  let sqPrint = 'X'
+  let sqPrint = 'X';
   for (let i = 0; i < args - 1; i++) {
-    sqPrint += 'X'
+    sqPrint += 'X';
   }
   while (args > 0) {
     console.log(sqPrint);
-	args--;
+    args--;
   }
 }
