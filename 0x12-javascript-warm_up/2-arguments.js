22fecf4ff3a98b0de377f4a3ec99e4aa2500c52d
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-13 22:16:00 +0100

    print command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 if (argv.length < 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else if (argv.length > 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
