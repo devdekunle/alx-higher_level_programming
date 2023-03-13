65cc4e8f61a64398052086b78ff6a47688d3fbbb
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-14 00:12:19 +0100

    use loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,7 @@ const args = process.argv;
 if (args.length < 3 || isNaN(args[2])) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < args[2]; i++)
-	console.log('C is fun');
+  for (let i = 0; i < args[2]; i++) {
+    console.log('C is fun');
+  }
 }
