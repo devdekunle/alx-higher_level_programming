df127deb5c809ca9002a11ece95f40cd1371415a
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-14 20:04:16 +0100

    computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+function factorial (n) {
+	if (n <= 0) {
+	  return 0;
+	}
+	if (n === 1) {
+	  return 1;
+
+	} else {
+	    return (n * factorial(n - 1));
+		}
+
+}
+const value = parseInt(process.argv[2]);
+if (isNaN(value)) {
+  console.log(1);
+} else {
+    console.log(factorial(value));
+}
