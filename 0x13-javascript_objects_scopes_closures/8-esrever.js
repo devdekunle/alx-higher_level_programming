3d8e61c661d14b9a78116361f2e2b4dcff60c479
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-16 16:31:56 +0100

    prints the rectangle using the character cvim

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+Write a function that returns the reversed version of a list:
+
+Prototype: exports.esrever = function (list)
+You are not allow to use the built-in method reverse
+*/
+
+exports.esrever = function (list) {
+  if (list === undefined) {
+  return null;
+  } else {
+  new_list = [];
+  size_list = list.length;
+  while (size_list => 0) {
+  list.push(list[size]);
+  --size_list;
+  }
+
+
+  }
+
+}
