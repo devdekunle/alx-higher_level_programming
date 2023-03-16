d5b6e7d2e7c05d233cb155c6c82c2fe75d1c7551
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-16 06:36:54 +0100

    check if w or h is a positive integer

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -10,9 +10,7 @@ If w or h is equal to 0 or not a positive integer, create an empty object
 */
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      const newObject = {};
-    } else {
+    if (w > 0 && h > 0 && w !== undefined && h !== undefined) {
       this.width = w;
       this.height = h;
     }
