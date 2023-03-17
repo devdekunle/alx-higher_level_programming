611237190547d992a2ada2893ff68a37b0ec8aa5
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-17 15:33:57 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     Access the database and print all states
     in the states table
     '''
-    db = MySQLdb.connect(host='localhost', port=3306,
-         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
     db_cur = db.cursor()
     db_cur.execute('SELECT * FROM states ORDER BY states.id ASC')
     rows = db_cur.fetchall()
-
 
     for row in rows:
         print(row)
