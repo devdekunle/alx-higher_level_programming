3a051b1dfc4279e7b48b667beb388cd0e82d9a93
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-17 20:47:27 +0100

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Write a script that takes in an argument
-and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
 Your script should take 4 arguments:
 mysql username, mysql password, database name and
 state name searched (no argument validation needed)
