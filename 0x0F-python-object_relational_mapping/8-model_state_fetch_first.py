0b5eff63f3d851f1bb52f907fb3da3ec9a1636fd
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-19 09:35:06 +0100

    fix empty list

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ a script that prints the first State
+ object from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    list the first state
+    """
+    db_uri = ('mysql+mysqldb://{}:{}@localhost/{}'.format
+              (argv[1], argv[2], argv[3]))
+
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    result = session.query(State).filter(State.id == 1)
+    if result == None:
+        print('Nothing')
+    for instance in result:
+        print(f"{instance.id}: {instance.name}")
