3415dd67623635df7e754e4c1289759c69ce282b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-30 19:55:56 +0100

    print size of http response content-length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # a script that takes in a url and displays the size of the body of the response
-if [ "$#" -eq 1 ]; then
-
-    url="$1"
-    response=$(curl -sI "$url")
-    body_size=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' |tr -d '\r')
-    echo "$body_size"
-fi
+curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
