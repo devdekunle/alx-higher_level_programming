103a59dfd2ab3b4c62fc0b1984a568cb7256a938
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-01 00:50:34 +0100

    send an email parameter using to a server using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+
+The email must be sent in the variable email
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to error check arguments passed to the script (number or type)
+Please test your script in the sandbox provided,
+using the web server running on port 5000
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    data = {}
+    data['email'] = argv[2]
+    response = requests.post(argv[1], params=data)
+    print(response.text)
