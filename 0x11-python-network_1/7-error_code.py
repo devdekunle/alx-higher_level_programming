e37f4fa9a11aae9e3ebf56f57fe1fa95e62c186f
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-01 01:20:35 +0100

    Error handling with requests object

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error: {response.status_code}")
+
+    else:
+        print(response.text)
