02907f24ac32043a2d1f15407192ad5fcd3fb551
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-01 14:30:49 +0100

    display githu credentials usong REST API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url= f"https://api.github.com/users/{argv[1]}"
+    header = {}
+    header['Accept'] = 'application/vnd.github+json'
+    header['X-GitHub-Api_version'] = "2022-11-28"
+    header['Authorization'] = f"Bearer {argv[2]}"
+
+    response = requests.get(url, headers=header)
+    print(response.json().get('id'))
