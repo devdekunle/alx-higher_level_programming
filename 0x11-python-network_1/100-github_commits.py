cd53a98f25a86fb01405e7e9bbf871cf731fcb99
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-02 01:02:28 +0100

    get git commit details from github api

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
 
     i = 0
     while i < 10:
-        print(f"{json_response[i].get('sha')}: {json_response[i].get('commit').get('author').get('name')}")
+        print(json_response[i].get('sha'), end=": "
+        print(json_response[i].get('commit').get('author').get('name'))
 
         i += 1
