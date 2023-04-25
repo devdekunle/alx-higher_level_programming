99e40e9f1c1a11d69e6e24803aa2f73c0a094942
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-25 14:07:42 +0100

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-/** Read and print the content of a file in node**/
+// Read and print the content of a file in node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
     return;
