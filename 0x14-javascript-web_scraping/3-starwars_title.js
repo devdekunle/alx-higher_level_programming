1a49fa2a6ee65b1b0a7c0b94ece2f30b13439d0f
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-25 20:29:34 +0000

    prints title of star wars movie based on episode id

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* script that prints the title of a Start Wars movie */
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
