08f123d6e175c5cd1872cdc554398a3d700b37d6
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-25 19:23:14 +0000

    get status code of a get http request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* script that display the status code of a GET request */
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code: ' + response.statusCode);
+});
