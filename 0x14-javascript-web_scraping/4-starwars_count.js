c74d82cf01236bb31ffaa845ae43df624dd34e96
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-25 21:35:29 +0000

    print number of movies where a character is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@
 character "Wedge Antilles" is present. */
 const request = require('request');
 const url = process.argv[2];
-const peopleUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
@@ -13,7 +12,8 @@ request(url, (error, response, body) => {
   let count = 0;
   for (let i = 0; i < json.results.length; i++) {
     for (let j = 0; j < json.results[i].characters.length; j++) {
-      if (json.results[i].characters[j] === peopleUrl) {
+      const charId = json.results[i].characters[j].split('/')[5];
+      if (charId === '18') {
         count++;
       }
     }
