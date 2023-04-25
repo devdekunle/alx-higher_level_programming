241d3d88a6e4798f3d3cba1c3d09781aad16f896
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-25 22:16:45 +0000

    get contents of script and save to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-/*Script that gets the content of a web page and save it in a file*/
+/* Script that gets the content of a web page and save it in a file */
 const request = require('request');
 const fs = require('fs');
-webUrl = process.argv[2];
-filePath = process.argv[3];
+const webUrl = process.argv[2];
+const filePath = process.argv[3];
 
-request(webUrl, function(error, response, body) {
-    if (error) {
-        console.log(error);
+request(webUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(filePath, body, 'utf-8', err => {
+    if (err) {
+      console.log(err);
     }
-    body = JSON.parse(body)
-    fs.writeFile(filePath, body, 'utf-8', err => {
-        if (err) {
-            console.log(err);
-        }
-    });
+  });
 });
